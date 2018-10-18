87c25d1b8bff7709e32f27891001dd778b436892
Tomasz Gradowski <tomgrad@gmail.com>
2018-10-18 21:30:54 +0200

    cleanup of include section

--- thread_pool.h
+++ thread_pool.h
@@ -1,15 +1,9 @@
 #ifndef THREAD_POOL_H
 #define THREAD_POOL_H
 
-#include <vector>
 #include <queue>
-#include <memory>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
 #include <future>
 #include <functional>
-#include <stdexcept>
 
 class ThreadPool
 {
